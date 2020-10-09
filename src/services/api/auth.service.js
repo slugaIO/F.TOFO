def080f7ec1075c7d563f05dd23022d25703cfa2
thomas <61018578+thomas-vlyby@users.noreply.github.com>
2020-10-09 16:33:25 +0200

    auth in dashboard

--- src/services/api/auth.service.js
+++ src/services/api/auth.service.js
@@ -47,7 +47,38 @@ class AuthService{
         });
     }
     setAuthCookieData = (cookieData) => {
-        localStorage.setItem(COOKIE_ID, JSON.stringify(cookieData));
+        localStorage.setItem(this.COOKIE_ID, JSON.stringify(cookieData));
+    }
+    authCheck = () => {
+        const API_URL    = `${this.API_URL}/api/user/token-validation`;
+        const cookieData = JSON.parse(localStorage.getItem(this.COOKIE_ID));
+        console.table(cookieData.token);
+        const data       = JSON.stringify({"refreshToken":`${cookieData.token.refreshToken}`});
+        const config  = {
+            method:'post',
+            url:API_URL,
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            data:data
+        };
+        return axios(config)
+        .then( (response) => {
+            if(response.data.success){
+                return new Promise( (resolve, reject) => {
+                    resolve({
+                        success:true   
+                    });
+                });
+            }
+        })
+        .catch( (error) => {
+            return new Promise((resolve, reject) => {
+                reject({
+                    success:false
+                })
+            })
+        });
     }
 }
 
