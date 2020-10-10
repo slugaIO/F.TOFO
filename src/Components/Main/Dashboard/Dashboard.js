8c5fbc1896f137b29e8a5983f5c8145bf4575e70
thomas <61018578+thomas-vlyby@users.noreply.github.com>
2020-10-10 21:11:21 +0200

    ...

--- src/Components/Main/Dashboard/Dashboard.js
+++ src/Components/Main/Dashboard/Dashboard.js
@@ -6,14 +6,21 @@ import {Redirect} from 'react-router-dom'
 class Dashboard extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            check:false
+        }
     }
     componentDidMount(){
 
     }
-    render() {
+    componentDidUpdate(){
+        console.log("Component Update");
+    }
+    render(){
         return(
             <React.Fragment>
             { this.props.isLoggedIn === false ? <Redirect to='/' />:null }
+            { this.state.check === true ? <h1>TRUE</h1>:<h1>FALSE</h1>}
             <h1>Dashboard {this.props.isLoggedIn === true ? 'JA' : 'NEIN'}</h1>
             </React.Fragment>
         )
