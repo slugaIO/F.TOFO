c5bee7796e1f2a4871b6f5e1598a953a4405a55e
thomas <61018578+thomas-vlyby@users.noreply.github.com>
2020-10-13 20:59:27 +0200

    new layout

--- src/Components/navigation/menu.js
+++ src/Components/navigation/menu.js
@@ -10,28 +10,38 @@ import {
   } from "react-router-dom";
   import { Navbar,Nav,NavDropdown,Form,FormControl,Button,InputGroup } from 'react-bootstrap'
   import {withRouter} from 'react-router-dom'
+  import { PersonFill,KeyFill } from 'react-bootstrap-icons';
 
 class TopNavigation extends React.Component{
     render(){
         return(
-            <Navbar className="bg-light justify-content-between">
-            <Form inline>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="Username"
-                  aria-label="Username"
-                  aria-describedby="basic-addon1"
-                />
-              </InputGroup>
-            </Form>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
-              <Button type="submit">Submit</Button>
-            </Form>
-          </Navbar>
+          <Navbar bg="dark" variant="dark">
+          <Nav className="mr-auto"> </Nav>
+          <Form inline>
+          <InputGroup  ml-auto>
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1"><PersonFill /></InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            className="mr-sm-2"
+          />
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1"><KeyFill/></InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="password"
+            aria-label="password"
+            aria-describedby="basic-addon1"
+            className="mr-sm-2"
+          />
+          <Button variant="info"  className="mr-sm-2">Login</Button>
+          <Button variant="success"  className="mr-sm-2">Register</Button>
+        </InputGroup>
+          </Form>
+        </Navbar>
         )  
     }
 }
