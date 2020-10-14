6289a39d180d150d4475ba59352b5275de65e989
thomas <61018578+thomas-vlyby@users.noreply.github.com>
2020-10-14 22:05:26 +0200

    css for sidebar

--- src/Components/navigation/sidebar.js
+++ src/Components/navigation/sidebar.js
@@ -2,6 +2,56 @@ import React from 'react';
 import Sidebar from "react-sidebar";  
 
 class TaskSideBar extends React.Component{
+    style = {
+        root: {
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          overflow: "hidden"
+        },
+        sidebar: {
+          zIndex: 2,
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          transition: "transform .3s ease-out",
+          WebkitTransition: "-webkit-transform .3s ease-out",
+          willChange: "transform",
+          overflowY: "auto",
+          backgroundColor:"white",
+          padding:"5%"
+        },
+        content: {
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          overflowY: "auto",
+          WebkitOverflowScrolling: "touch",
+          transition: "left .3s ease-out, right .3s ease-out"
+        },
+        overlay: {
+          zIndex: 1,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          opacity: 0.5,
+          visibility: "hidden",
+          transition: "opacity .3s ease-out, visibility .3s ease-out",
+          backgroundColor: "rgba(0,0,0,.3)"
+        },
+        dragHandle: {
+          zIndex: 1,
+          position: "fixed",
+          top: 0,
+          bottom: 0
+        }
+    };
     render(){
         if(!this.props.isLoggedIn){
             return (
@@ -13,7 +63,7 @@ class TaskSideBar extends React.Component{
             sidebar={<b>Sidebar content</b>}
             open={this.props.sidebarOpen}
             onSetOpen={this.props.onSetSidebarOpen}
-            styles={{ sidebar: { background: "white" } }}
+            styles={this.style}
         >
         <button onClick={() => this.props.onSetSidebarOpen(true)}>
             Open sidebar
