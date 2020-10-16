bf474610a917bd0bf743c59d0d156d6139e15384
thomas <61018578+thomas-vlyby@users.noreply.github.com>
2020-10-16 22:43:05 +0200

    npm react-date-time-picker test

--- src/Components/Main/Dashboard/Task/create-task.js
+++ src/Components/Main/Dashboard/Task/create-task.js
@@ -1,6 +1,8 @@
+var DatePicker = require("react-bootstrap-date-picker");
 import React from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, Container} from 'react-bootstrap';
 import {Form} from 'react-bootstrap';
+
 
 import AuthService from '../../../../services/api/auth.service'
 
@@ -15,6 +17,19 @@ class CreateTask extends React.Component{
     }
     handleChange = e => {
         this.setState({ [e.target.name]: e.target.value });
+    }
+    // ? needed by date-picker
+    getInitialState = () => {
+        var value = new Date().toISOString();
+        return {
+          value: value
+        }
+    }
+    // ? needed by date-picker 
+    componentDidMount(){
+        const hiddenInputElement = document.getElementById("example-datepicker");
+        console.log(hiddenInputElement.value); // ISO String, ex: "2016-11-19T12:00:00.000Z"
+        console.log(hiddenInputElement.getAttribute('data-formattedvalue')) // Formatted String, ex: "11/19/2016"
     }
     addTask  = event => {
         // get new Access Token
@@ -44,20 +59,31 @@ class CreateTask extends React.Component{
         event.preventDefault();
     }
     render(){
+        const style = {
+            container:{
+                backgroundColor:'#ccc'
+            }
+        }
         return(
-            <Form>
-            <Form.Group controlId="addTask">
-              <Form.Label>Title</Form.Label>
-              <Form.Control name='title' value={this.state.title} type="text" onChange={this.handleChange}/>
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Task</Form.Label>
-                <Form.Control name='taskContent' as="textarea" value={this.state.taskContent} rows="3" onChange={this.handleChange}/>
-            </Form.Group>
-            <Button variant="success" type="submit" onClick={this.addTask}>
-              add Task
-            </Button>
-          </Form>
+            <Container fluid style={style.container}>
+                <Form>
+                <Form.Group controlId="addTask">
+                <Form.Label>Title</Form.Label>
+                <Form.Control name='title' value={this.state.title} type="text" onChange={this.handleChange}/>
+                </Form.Group>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Task</Form.Label>
+                    <Form.Control name='taskContent' as="textarea" value={this.state.taskContent} rows="3" onChange={this.handleChange}/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Label</Form.Label>
+                    <DatePicker id="example-datepicker" value={this.state.value} onChange={this.handleChange} />
+                </Form.Group>;
+                <Button variant="success" type="submit" onClick={this.addTask}>
+                add Task
+                </Button>
+            </Form>
+            </Container>
         )
     }
 }
